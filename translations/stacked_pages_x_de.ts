6524ecd1117e17eb3dc1412232ed08dc024f01c1
ekkes-corner <ekke@ekkes-corner.org>
2021-07-06 20:25:52 +0200

    ts

--- translations/stacked_pages_x_de.ts
+++ translations/stacked_pages_x_de.ts
@@ -2,99 +2,323 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
+    <name>PageFive</name>
+    <message>
+        <location filename="../pages/PageFive.qml" line="15"/>
+        <source>Page 5 of 5 in Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFive.qml" line="24"/>
+        <source>Hello Five</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFive.qml" line="34"/>
+        <source>This is page 5 of 5 - the top most page
+
+Navigate back to previous page:
+* Button labeled &apos;POP&apos;
+* Back Button placed top-left in Titlebar
+* on Android: System Back Button at bottom left
+
+Navigate back to the root using the FAB
+
+Jump back to &apos;PageThree&apos; using Button &apos;BACK TO 3&apos;
+&apos;PageFour&apos; will automatically pop()
+
+Button &apos;FIND 3&apos; tries to find &apos;PageThree&apos; by property &apos;name&apos;.
+Find will only work if page is instantiated. More details in blog entry.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFive.qml" line="55"/>
+        <source>PageThree not found in StackView</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFive.qml" line="78"/>
+        <source>Message from Page Five</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFive.qml" line="82"/>
+        <source>Init done from Five</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFive.qml" line="86"/>
+        <source>Cleanup done from Five</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PageFour</name>
+    <message>
+        <location filename="../pages/PageFour.qml" line="15"/>
+        <source>Page 4 of 5 in Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFour.qml" line="24"/>
+        <source>Hello Four</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFour.qml" line="34"/>
+        <source>This is page 4 of 5
+
+Navigate back to previous page:
+* Button labeled &apos;POP&apos;
+* Back Button placed top-left in Titlebar
+* on Android: System Back Button at bottom left
+
+Navigate forward to the next page &apos;PageFive&apos; using the FAB or Button &apos;PUSH 5&apos;
+
+Below the buttons a text is displayed:
+* &apos;Message from Page Three&apos; or
+* &apos;n/a&apos; if PageThree is not loaded because we &apos;jumped&apos; from 2 to 5
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFour.qml" line="72"/>
+        <source>Message from Page Four</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFour.qml" line="76"/>
+        <source>Init done from Four</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageFour.qml" line="80"/>
+        <source>Cleanup done from Four</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PageOne</name>
+    <message>
+        <location filename="../pages/PageOne.qml" line="15"/>
+        <source>Root Page in Stack of max 5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageOne.qml" line="24"/>
+        <source>Hello Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageOne.qml" line="34"/>
+        <source>This is the root page of a StackView - the &apos;initial Item&apos;
+
+From here you can navigate forward to the next page using the &apos;FloatingActionButton&apos; (FAB) placed at bottom-right or using the Raised Button named &apos;PUSH 2&apos;
+
+This StackView can stack a maximum of 5 pages including this root page.
+
+From &apos;Option Menu&apos;(three dots) placed top right in ToolBar you can switch Theme and change primary / accent
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageOne.qml" line="63"/>
+        <source>Message from Page One</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageOne.qml" line="67"/>
+        <source>Init done from One</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageOne.qml" line="71"/>
+        <source>Cleanup done from One</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PageThree</name>
+    <message>
+        <location filename="../pages/PageThree.qml" line="15"/>
+        <source>Page 3 of 5 in Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageThree.qml" line="24"/>
+        <source>Hello Three</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageThree.qml" line="34"/>
+        <source>This is page 3 of 5
+Here we&apos;re using another kind of Material Buttons: Flat Buttons
+
+Navigate back to previous page:
+* Button labeled &apos;POP&apos;
+* Back Button placed top-left in Titlebar
+* on Android: System Back Button at bottom left
+
+Navigate forward to the next page &apos;PageFour&apos; using the FAB or Button &apos;PUSH 4&apos;
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageThree.qml" line="65"/>
+        <source>Message from Page Three</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageThree.qml" line="69"/>
+        <source>Init done from Three</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageThree.qml" line="73"/>
+        <source>Cleanup done from Three</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PageTwo</name>
+    <message>
+        <location filename="../pages/PageTwo.qml" line="15"/>
+        <source>Page 2 of 5 in Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageTwo.qml" line="24"/>
+        <source>Hello Two</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageTwo.qml" line="34"/>
+        <source>This is page 2 of 5
+
+Navigate back to previous page:
+* Button labeled &apos;POP&apos;
+* Back Button placed top-left in Titlebar
+* on Android: System Back Button at bottom left
+
+Navigate forward to the next page &apos;PageThree&apos; using the FAB or Button &apos;PUSH 3&apos;
+
+Button labeled GOTO 5 jumps up to the top most page: PageFive. In this case PageThree and PageFour are NOT loaded - only Components are placed as placeholders in stack
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageTwo.qml" line="66"/>
+        <source>Message from Page Two</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageTwo.qml" line="70"/>
+        <source>Init done from Two</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pages/PageTwo.qml" line="74"/>
+        <source>Cleanup done from Two</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>PopupPalette</name>
     <message>
-        <location filename="../common/PopupPalette.qml" line="71"/>
+        <location filename="../common/PopupPalette.qml" line="74"/>
         <source>Material Red</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="72"/>
+        <location filename="../common/PopupPalette.qml" line="75"/>
         <source>Material Pink</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="73"/>
+        <location filename="../common/PopupPalette.qml" line="76"/>
         <source>Material Purple</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="74"/>
+        <location filename="../common/PopupPalette.qml" line="77"/>
         <source>Material DeepPurple</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="75"/>
+        <location filename="../common/PopupPalette.qml" line="78"/>
         <source>Material Indigo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="76"/>
+        <location filename="../common/PopupPalette.qml" line="79"/>
         <source>Material Blue</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="77"/>
+        <location filename="../common/PopupPalette.qml" line="80"/>
         <source>Material LightBlue</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="78"/>
+        <location filename="../common/PopupPalette.qml" line="81"/>
         <source>Material Cyan</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="79"/>
+        <location filename="../common/PopupPalette.qml" line="82"/>
         <source>Material Teal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="80"/>
+        <location filename="../common/PopupPalette.qml" line="83"/>
         <source>Material Green</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="81"/>
+        <location filename="../common/PopupPalette.qml" line="84"/>
         <source>Material LightGreen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="82"/>
+        <location filename="../common/PopupPalette.qml" line="85"/>
         <source>Material Lime</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="83"/>
+        <location filename="../common/PopupPalette.qml" line="86"/>
         <source>Material Yellow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="84"/>
+        <location filename="../common/PopupPalette.qml" line="87"/>
         <source>Material Amber</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="85"/>
+        <location filename="../common/PopupPalette.qml" line="88"/>
         <source>Material Orange</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="86"/>
+        <location filename="../common/PopupPalette.qml" line="89"/>
         <source>Material DeepOrange</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="87"/>
+        <location filename="../common/PopupPalette.qml" line="90"/>
         <source>Material Brown</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="88"/>
+        <location filename="../common/PopupPalette.qml" line="91"/>
         <source>Material Grey</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/PopupPalette.qml" line="89"/>
+        <location filename="../common/PopupPalette.qml" line="92"/>
         <source>Material BlueGrey</source>
         <translation type="unfinished"></translation>
     </message>
@@ -102,36 +326,54 @@
 <context>
     <name>SimpleTextTitle</name>
     <message>
-        <location filename="../common/SimpleTextTitle.qml" line="38"/>
+        <location filename="../common/SimpleTextTitle.qml" line="39"/>
         <source>Light Theme</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/SimpleTextTitle.qml" line="38"/>
+        <location filename="../common/SimpleTextTitle.qml" line="39"/>
         <source>Dark Theme</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/SimpleTextTitle.qml" line="44"/>
-        <source>Headline Accent Color</source>
+        <location filename="../common/SimpleTextTitle.qml" line="45"/>
+        <source>Select Primary Color</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../common/SimpleTextTitle.qml" line="44"/>
-        <source>Headline Primary Color</source>
+        <location filename="../common/SimpleTextTitle.qml" line="52"/>
+        <source>Select Accent Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StackTextTitle</name>
+    <message>
+        <location filename="../common/StackTextTitle.qml" line="52"/>
+        <source>Light Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common/StackTextTitle.qml" line="52"/>
+        <source>Dark Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common/StackTextTitle.qml" line="58"/>
+        <source>Select Primary Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common/StackTextTitle.qml" line="65"/>
+        <source>Select Accent Color</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="85"/>
+        <location filename="../main.qml" line="87"/>
         <source>A simple Stacked - Pages APP</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../main.qml" line="100"/>
-        <source>Hello Stack</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
